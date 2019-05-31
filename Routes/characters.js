e3ce5ac4753ea34b4946ea4b3b5c70b31e069b52
jennydang <jenny.dang117@gmail.com>
2019-05-31 20:10:49 +0200

    refactoring codes & test in character sorting

--- Routes/characters.js
+++ Routes/characters.js
@@ -5,115 +5,111 @@ const router = new Router()
 const baseUrl = 'https://swapi.co/api/films/'
 
 // Search movie by title => characters => filter "gender" => sort "height" OR "age"
-router.get('/movies/search', (req, res) => {
+router.get('/movies/search', (req, res, next) => {
     let { title, gender, sortHeight, sortAge } = req.query
-    // console.log(title)
-    // console.log(gender)
-    // console.log(sortHeight)
-    // console.log(sortAge)
+    
     axios
         .get(baseUrl)
         .then(response => response.data)
-        .then(data => {
+        .then(data => data.results)
+        .then(movies => {
             if(title === undefined){
-                return res.status(400).send("Bad request!")
+                res.status(400).send({ message: "Bad request!"})
+            }
+            else if (title === ""){
+                res.status(400).send({ message: "Title search term should not be empty!"})
             }
             else {
-                if(title !== ""){
-                    const promisesMovies = Promise.all(data.results)
-                        .then(movies => {
-                            // data = [{movie1},{movie2},...]
-                            const filterMovie = movies.filter(movie => {
-                                const movieTitle = movie.title.toLowerCase()
-                                const titleQuery = title.toLowerCase()
-                                return movieTitle.includes(titleQuery)
-                            })
+                const titleQuery = title.toLowerCase()
+                const movieFound =  movies.find(movie => {
+                    const movieTitle = movie.title.toLowerCase()
+                    if(movieTitle.includes(titleQuery)){
+                        return movie 
+                    }
+                })
 
-                            if(filterMovie.length === 0){
-                                return res.status(404).send("Movie not found!!")
-                            }else {
-                                return filterMovie[0]
-                            }
-                        })
-                        .then(charactersByMovie => charactersByMovie.characters)
-                        .then(characters => {
-                                const promisesCharacter =  Promise.all(characters.map(character => {
-                                    return axios.get(character)
-                                                .then(response => response.data)    
-                                }))    
-                                return promisesCharacter
-                        })
-                        .then(results => {
-                            //results is [{characterObj1},{characterObj2},...]
-                            if(gender !== undefined){
-                                let genderQueryValue = gender.toLowerCase()
-                                if(genderQueryValue === ""){
-                                    return res.status(400).send("Bad request!")
-                                }else if(genderQueryValue !== 'male' && genderQueryValue !== 'female') {    
-                                    return res.status(400).send("Bad request!")
-                                } else {
-                                    const genderFilter = results.filter(result => genderQueryValue === result.gender)       
-                                    return genderFilter
-                                } 
-                            } else {
-                                return res.json(results)
-                            }
-                        }) 
-                        .then(charsByGender => {
-                            if(sortHeight === undefined && sortAge === undefined){
-                                return res.json(charsByGender)
-                            }
-                            else if (sortHeight !== undefined && sortAge === undefined){
-                                let sortHeightQueryValue = sortHeight.toLowerCase()
-                                if(sortHeightQueryValue === ""){
-                                    return res.status(400).send("Bad request!")
-                                } else if(sortHeightQueryValue !== 'asc' && sortHeightQueryValue !== 'desc') {    
-                                    return res.status(400).send("Bad request!")
-                                } else {
-                                    const charsSortHeight = charsByGender.sort((a,b) => {
-                                        if(sortHeightQueryValue === 'asc'){
-                                            return Number(a.height) - Number(b.height)
-                                        } else {
-                                            return Number(b.height) - Number(a.height)
-                                        }
-                                    })
-                                    return res.json(charsSortHeight)
-                                }
-                            }
-                            else if (sortHeight === undefined && sortAge !== undefined){
-                                let sortAgeQueryValue = sortAge.toLowerCase()
-                                if(sortAgeQueryValue === ""){
-                                    return res.status(400).send("Bad request!")
-                                } else if(sortAgeQueryValue !== 'asc' && sortAgeQueryValue !== 'desc') {    
-                                    return res.status(400).send("Bad request!")
-                                } else {
-                                    const charsSortAge = charsByGender.sort((a,b) => {
-                                        if(sortAgeQueryValue === 'asc'){
-                                            return parseInt(a.birth_year) - parseInt(b.birth_year)
-                                        } else {
-                                            return parseInt(b.birth_year) - parseInt(a.birth_year)
-                                        }
-                                    })
-                                    return res.json(charsSortAge)
-                                }
-                            }
-                            else if (sortHeight !== undefined && sortAge !== undefined){
-                                return res.status(400).send("You can sort either height or age per time")
-                            }
-                            else {
-                                return res.json(charsByGender)
-                            }
+                if(movieFound === undefined){
+                    res.status(404).send({ message: `Not found any movie with ${titleQuery} title `})
+                }
+                else {
+                    
+                    const characters = movieFound.characters
+                    
+                    const charactersPromises =  characters.map(url => axios.get(url))
+    
+                    return Promise.all(charactersPromises).then(responses => {
+                        const people = responses.map(response => response.data)
+        
+                        if(gender === undefined){
+                            res.json(people)
+                        }
+                        else if (gender === ""){
+                            res.status(400).send({ message: "Search term should not be empty!"})
+                        }
+                        else {
+                            const genderQuery = gender.toLowerCase()
+                            const listPeopleByGender =  people.filter(character => character.gender === genderQuery)
                             
-                        })
-                        .catch(err => console.error(err)) 
+                            if(listPeopleByGender.length === 0){
+                                res.status(404).send({ message: `Not found characters with that search term!`})
+                            } else {
+                                //List of people by filter gender (female/male)
+                                //SORTING Height OR Age here
 
-                    return promisesMovies                 
-                } else {
-                    return res.status(400).json({ message: "It is bad request, you should enter the title"})
+                                if(sortHeight === undefined && sortAge === undefined){
+                                    res.json(listPeopleByGender)
+                                } 
+                                else if (sortHeight !== undefined && sortAge === undefined){
+                                    let sortHeightQuery = sortHeight.toLowerCase()
+                                    if(sortHeightQuery === ""){
+                                        res.status(400).send({ message: "Search term should not be empty!"})
+                                    } 
+                                    else if(sortHeightQuery !== 'asc' && sortHeightQuery !== 'desc') {    
+                                        res.status(400).send({ message: "Bad request!"})
+                                    } 
+                                    else {
+                                        const charsSortHeight = listPeopleByGender.sort((a,b) => {
+                                            if(sortHeightQuery === 'asc'){
+                                                return Number(a.height) - Number(b.height)
+                                            } else {
+                                                return Number(b.height) - Number(a.height)
+                                            }
+                                        })
+                                        res.json(charsSortHeight)
+                                    }
+                                }
+                                else if(sortHeight === undefined && sortAge !== undefined){
+                                    let sortAgeQuery = sortAge.toLowerCase()
+                                    if(sortAgeQuery === ""){
+                                        res.status(400).send({ message: "Search term should not be empty!"})
+                                    } 
+                                    else if(sortAgeQuery !== 'asc' && sortAgeQuery !== 'desc') {    
+                                        res.status(400).send({ message: "Bad request!"})
+                                    } 
+                                    else {
+                                        const charsSortAge = listPeopleByGender.sort((a,b) => {
+                                            if(sortAgeQuery === 'asc'){
+                                                return parseInt(a.birth_year) - parseInt(b.birth_year)
+                                            } else {
+                                                return parseInt(b.birth_year) - parseInt(a.birth_year)
+                                            }
+                                        })
+                                        res.json(charsSortAge)
+                                    }
+                                }
+                                else if (sortHeight !== undefined && sortAge !== undefined){
+                                    res.status(400).send({ message: "You can sort either height or age per time"})
+                                }
+                            }
+
+                        }
+                    })
+                    .catch(err => console.error(err))
                 }
             }
-        })  
+        }) 
         .catch(err => console.error(err)) 
+                
 })
 
 
